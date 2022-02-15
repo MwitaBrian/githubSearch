c4491d747e5dfab2abc00cb1c858af1b17b821a2
MwitaBrian <brianmwita13@gmail.com>
2022-02-15 03:14:03 +0300

    Change username to user

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -6,15 +6,16 @@ import { map } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class ProfileService {
-  private username: string;
+  private user: string;
+  private apiUrl: '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';
 
   constructor(private http: HttpClient ) {
     console.log("Service is now ready");
-    this.username = 'MwitaBrian'
+    this.user = 'MwitaBrian'
    }
 
    getProfileInfo(){
-     return this.http.get<any[]>('https://api.github.com/users/' + this.username)
+     return this.http.get<any[]>('https://api.github.com/users/' + this.user + "&api_Url=" + this.apiUrl)
      .pipe(map(res => res));
    }
 }
