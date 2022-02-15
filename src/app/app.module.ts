39fbfe694c7a942cc835ee9fdd4d61a636a71a41
MwitaBrian <brianmwita13@gmail.com>
2022-02-15 04:45:16 +0300

    Add and Display navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { GithubComponent } from './github/github.component';
 import { HttpClientModule } from '@angular/common/http'
 import { ProfileService } from './services/profile.service';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GithubComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
